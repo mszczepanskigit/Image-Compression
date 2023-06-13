1c59315538eff8491f1a2ebed39710b33f6027c4
mszczepanskigit <mateusz.szczepanski.kontakt@gmail.com>
2023-06-13 22:24:57 +0200

    quantization is done, fff_90_museum added

--- fourier_transform_thresholding.py
+++ fourier_transform_thresholding.py
@@ -2,6 +2,7 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 import copy as cp
+import cv2
 
 
 def thresholding_fft(A, thresh):
@@ -23,7 +24,7 @@ if __name__ == "__main__":
     boat_fft = np.fft.fft2(museum)
     name = "museum"
 
-    boat_fft_10 = thresholding_fft(boat_fft, 10)
+    """boat_fft_10 = thresholding_fft(boat_fft, 10)
     boat_recovered_10 = np.abs(np.fft.ifft2(boat_fft_10)).astype(np.uint8)
     boat_fft_10 = Image.fromarray(boat_recovered_10)
     boat_fft_10.save(f'images/{name}_fft_10.png')
@@ -41,7 +42,12 @@ if __name__ == "__main__":
     boat_fft_95 = thresholding_fft(boat_fft, 95)
     boat_recovered_95 = np.abs(np.fft.ifft2(boat_fft_95)).astype(np.uint8)
     boat_fft_95 = Image.fromarray(boat_recovered_95)
-    boat_fft_95.save(f'images/{name}_fft_95.png')
+    boat_fft_95.save(f'images/{name}_fft_95.png')"""
+
+    boat_fft_99 = thresholding_fft(boat_fft, 99)
+    boat_recovered_99 = np.abs(np.fft.ifft2(boat_fft_99)).astype(np.uint8)
+    boat_fft_99 = Image.fromarray(boat_recovered_99)
+    boat_fft_99.save(f'images/{name}_fft_99.png')
 
     # plt for testing purpose
     """plt.figure(figsize=(7, 7))
---
+++ quantization.py
@@ -0,0 +1,57 @@
+import numpy as np
+from PIL import Image
+import copy as cp
+
+
+def first_boat(A):
+    cp_A = cp.copy(A)
+    return 4 * np.floor(cp_A / 4)
+
+
+def second_boat(A):
+    cp_A = cp.copy(A)
+    return 64 * np.floor(cp_A / 64)
+
+
+def third_boat(A):
+    cp_A = cp.copy(A)
+    return 32 * (np.floor(cp_A / 64) + np.ceil(cp_A / 64))
+
+
+def fourth_boat(A):
+    cp_A = cp.copy(A)
+    cp_A[cp_A < 64] = 32
+    cp_A[np.logical_and(63 < cp_A, cp_A < 96)] = 80
+    cp_A[np.logical_and(95 < cp_A, cp_A < 112)] = 104
+    cp_A[np.logical_and(111 < cp_A, cp_A < 120)] = 116
+    cp_A[np.logical_and(119 < cp_A, cp_A < 124)] = 122
+    return cp_A
+
+
+if __name__ == "__main__":
+    # noinspection PyTypeChecker
+    # boat = np.asarray(Image.open('images/Boat.png').convert('RGB').convert('L'))
+    # noinspection PyTypeChecker
+    boat = np.asarray(Image.open('images/Museum.png').convert('L'))
+
+    # name = "boat"
+    name = "museum"
+
+    boat = boat.astype(np.double)
+
+    boat_quant_1 = first_boat(boat).astype(np.uint8)
+    boat_quant_2 = second_boat(boat).astype(np.uint8)
+    boat_quant_3 = third_boat(boat).astype(np.uint8)
+    boat_quant_4 = fourth_boat(boat).astype(np.uint8)
+
+    boat_quant_1 = Image.fromarray(boat_quant_1)
+    boat_quant_1.save(f'images/{name}_quant_1.png')
+
+    boat_quant_2 = Image.fromarray(boat_quant_2)
+    boat_quant_2.save(f'images/{name}_quant_2.png')
+
+    boat_quant_3 = Image.fromarray(boat_quant_3)
+    boat_quant_3.save(f'images/{name}_quant_3.png')
+
+    boat_quant_4 = Image.fromarray(boat_quant_4)
+    boat_quant_4.save(f'images/{name}_quant_4.png')
